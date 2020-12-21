cd629bf16af6e52c4a0439e4c8f4376b0045be1c
Puszka <kamilharaf7@gmail.com>
2020-12-21 20:30:49 +0100

    Initial Commit

---
+++ MainScene.js
@@ -0,0 +1,17 @@
+class MainScene extends Phaser.Scene {
+    constructor() {
+        super({key:"MainScene"});
+    }
+
+    create() {
+
+    }
+
+    preload() {
+        game.load.image("theme", "Assets/winter-theme.jpg");
+    }
+
+    update() {
+
+    }
+}
