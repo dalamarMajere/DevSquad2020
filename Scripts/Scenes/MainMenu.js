09dcf6468414c8460f9a4bec26a1d4afd4b67997
Puszka <kamilharaf7@gmail.com>
2021-01-04 17:05:02 +0100

    Music Stop Button

--- Scripts/Scenes/MainMenu.js
+++ Scripts/Scenes/MainMenu.js
@@ -1,14 +1,16 @@
 let highscore = 0;
 let playMusic = true;
+let audio = true;
+
 class MainMenu extends Phaser.Scene {
 
     constructor() {
-        super({key:"MainMenu"});
+        super({key: "MainMenu"});
     }
 
     preload() {
-        this.load.audio('backgroundMusic','Assets/Sounds/backgroundLoop.mp3');
-        this.load.audio('buttonClick','Assets/Sounds/buttonClick.mp3');
+        this.load.audio('backgroundMusic', 'Assets/Sounds/backgroundLoop.mp3');
+        this.load.audio('buttonClick', 'Assets/Sounds/buttonClick.mp3');
 
         this.titleScreenImage = this.load.image('MainMenuText', 'Assets/MainMenu/MainMenuText.png');
         this.load.image('bg1', 'Assets/Grass/background.png');
@@ -18,74 +20,119 @@ class MainMenu extends Phaser.Scene {
 
         this.load.atlas('transition', 'Assets/General/transition.png', 'Assets/General/transition.json');
 
-        this.playButton = this.load.image('playButton', 'Assets/MainMenu/PlayButton.png');
-        this.playButtonHover = this.load.image('playButtonHover', 'Assets/MainMenu/PlayButtonHover.png');
+        this.load.image('playButton', 'Assets/MainMenu/PlayButton.png');
+        this.load.image('playButtonHover', 'Assets/MainMenu/PlayButtonHover.png');
 
-        this.questionButton = this.load.image('questionButton', 'Assets/MainMenu/QuestionButton.png');
-        this.questionButtonHover = this.load.image('questionButtonHover', 'Assets/MainMenu/QuestionButtonHover.png');
+        this.load.image('questionButton', 'Assets/MainMenu/QuestionButton.png');
+        this.load.image('questionButtonHover', 'Assets/MainMenu/QuestionButtonHover.png');
+
+        this.load.image('audioActive', 'Assets/MainMenu/audioActiveButton.png');
+        this.load.image('audioActiveHover', 'Assets/MainMenu/audioActiveButtonHover.png');
+        this.load.image('audioInactive', 'Assets/MainMenu/audioInactiveButton.png');
+        this.load.image('audioInactiveHover', 'Assets/MainMenu/audioInactiveButtonHover.png');
+
     }
 
-    create(){
+    create() {
+        //delta time
         this.lastTime = 0;
+
+        //background
         this.backgroundTimer = 0;
         this.backgroundChangeTime = 4;
-
-        this.backgrounds = ['bg1','bg2','bg3','bg4'];
-        //background
-        this.background = this.add.sprite(0,0,this.backgrounds[Phaser.Math.Between(0, 3)]);
-        this.background.setOrigin(0,0);
+        this.backgrounds = ['bg1', 'bg2', 'bg3', 'bg4'];
+        this.background = this.add.sprite(0, 0, this.backgrounds[Phaser.Math.Between(0, 3)]);
+        this.background.setOrigin(0, 0);
         this.background.setDepth(0);
 
-        this.titleScreen = this.add.image(0,0,'MainMenuText');
-        this.titleScreen.setOrigin(0,0);
+        this.titleScreen = this.add.image(0, 0, 'MainMenuText');
+        this.titleScreen.setOrigin(0, 0);
         this.titleScreen.setDepth(10);
 
-
-        //play button
-        this.playButton = this.add.sprite(this.sys.game.config.width/2,this.sys.game.config.height/2,'playButton');
+        //buttons
+        this.playButton = this.add.sprite(this.sys.game.config.width / 2, this.sys.game.config.height / 2, 'playButton');
+        this.add.existing(this.playButton);
         this.playButton.setDepth(10);
-        this.playButtonWidth = this.playButton.width;
-        this.playButtonHeight = this.playButton.height;
-        this.playButtonPosX = this.playButton.x - this.playButtonWidth/2;
-        this.playButtonPosY = this.playButton.y - this.playButtonHeight/2;
+        this.playButton
+            .setInteractive()
+            .on('pointerover', () => {
+                this.playButton.setTexture('playButtonHover');
+            })
+            .on('pointerout', () => {
+                this.playButton.setTexture('playButton');
+            })
+            .on('pointerup', () => {
+                this.playButtonClick()
+            });
 
-
-        this.questionButton = this.add.sprite(this.sys.game.config.width/2,this.sys.game.config.height/2 + this.playButton.height + 20,'questionButton');
+        this.questionButton = this.add.sprite(this.sys.game.config.width / 2, this.sys.game.config.height / 2 + this.playButton.height + 20, 'questionButton');
+        this.add.existing(this.questionButton);
         this.questionButton.setDepth(10);
-        this.questionButtonWidth = this.questionButton.width;
-        this.questionButtonHeight = this.questionButton.height;
-        this.questionButtonPosX = this.questionButton.x - this.questionButtonWidth/2;
-        this.questionButtonPosY = this.questionButton.y - this.questionButtonHeight/2;
+        this.questionButton
+            .setInteractive()
+            .on('pointerover', () => {
+                this.questionButton.setTexture('questionButtonHover');
+            })
+            .on('pointerout', () => {
+                this.questionButton.setTexture('questionButton');
+            })
+            .on('pointerup', () => {
+                this.questionButtonClick()
+            });
 
-        if(highscore<score)
-            highscore=score;
+        this.audioButton = this.add.sprite(this.sys.game.config.width, this.sys.game.config.height, 'audioActive');
+        if(!audio)
+            this.audioButton.setTexture('audioInactive');
 
-        this.highScore = this.add.text(0,0,'Highscore: '+highscore.toString(),{ fontFamily: 'Arial', fontSize: '50px', color: '#ffffff' ,stroke: '#000000', strokeThickness:5 });
+        this.audioButton.setOrigin(0,0);
+        this.audioButton.x = this.audioButton.x - this.audioButton.width - 10;
+        this.audioButton.y = this.audioButton.y - this.audioButton.height - 10;
+
+        this.add.existing(this.audioButton);
+        this.audioButton.setDepth(10);
+        this.audioButton
+            .setInteractive()
+            .on('pointerover', () => {
+                if (audio) {
+                    this.audioButton.setTexture('audioActiveHover');
+                } else {
+                    this.audioButton.setTexture('audioInactiveHover');
+                }
+            })
+            .on('pointerout', () => {
+                if (audio) {
+                    this.audioButton.setTexture('audioActive');
+                } else {
+                    this.audioButton.setTexture('audioInactive');
+                }
+            })
+            .on('pointerup', () => {
+                this.audioButtonClick()
+            });
+
+
+        if (highscore < score)
+            highscore = score;
+
+        this.highScore = this.add.text(0, 0, 'Highscore: ' + highscore.toString(), {
+            fontFamily: 'Arial',
+            fontSize: '50px',
+            color: '#ffffff',
+            stroke: '#000000',
+            strokeThickness: 5
+        });
+
         this.highScore.setDepth(10);
-        //center and position the text;
-        this.highScore.x = this.sys.game.config.width/2 - this.highScore.displayWidth/2;
-        this.highScore.y = this.sys.game.config.height - this.highScore.displayHeight*2;
-        //click check
-        this.input.on('pointerdown',function(event){
-            if(this.isHoveringPlay){
-                NewGame();
-                this.click.play();
-                game.scene.stop("MainMenu");
-            }
-            if(this.isHoveringQuestion){
-                this.click.play();
-                game.scene.start("HowToPlay");
-                game.scene.stop("MainMenu");
-            }
-        }, this);
+        this.highScore.x = this.sys.game.config.width / 2 - this.highScore.displayWidth / 2;
+        this.highScore.y = this.sys.game.config.height / 2 - this.highScore.displayHeight * 3;
+
         //sound
         this.music = this.sound.add('backgroundMusic');
         this.music.loop = true;
         this.music.volume = 0.2;
-
         this.click = this.sound.add('buttonClick');
-        //transition
 
+        //transition
         this.anims.create({
             key: 'transitionAnimation',
             frames: this.anims.generateFrameNames('transition', {
@@ -99,67 +146,65 @@ class MainMenu extends Phaser.Scene {
         });
     }
 
-    update(delta){
-        if(playMusic){
-            this.music.play();
-            playMusic = false;
+    update(delta) {
+        if(audio){
+            if (playMusic) {
+                this.music.play();
+                playMusic = false;
+            }
         }
-        this.backgroundTimer += (delta - this.lastTime)/1000;
-        this.lastTime = delta;
 
-        if(this.backgroundTimer > this.backgroundChangeTime){
+
+        this.backgroundTimer += (delta - this.lastTime) / 1000;
+        this.lastTime = delta;
+        if (this.backgroundTimer > this.backgroundChangeTime) {
             this.backgroundTimer = 0;
-            this.transition = this.add.sprite(this.sys.game.config.width/2,this.sys.game.config.height/2,'transition');
+            this.transition = this.add.sprite(this.sys.game.config.width / 2, this.sys.game.config.height / 2, 'transition');
             this.transition.setDepth(5);
             this.transition.play('transitionAnimation');
             this.transitioning = true;
         }
 
-        if(this.transitioning){
-            if(!this.transition.visible){
+
+        if (this.transitioning) {
+            if (!this.transition.visible) {
                 this.transitioning = false;
                 let x = 0;
-                do{
+                do {
                     x = Phaser.Math.Between(0, 3);
-                }while(this.backgrounds[x] == this.background.texture.key)
+                } while (this.backgrounds[x] == this.background.texture.key)
 
                 this.background.setTexture(this.backgrounds[x]);
-                this.transition.visible=true;
+                this.transition.visible = true;
                 this.transition.playReverse('transitionAnimation');
             }
         }
-        //get mouse position
-        this.mousePosX = game.input.mousePointer.x;
-        this.mousePosY = game.input.mousePointer.y;
-        this.isHoveringPlay = false;
-        this.isHoveringQuestion = false;
+    }
 
-        //check if the mouse is hovering over the play button
-        if(this.mousePosX > this.playButtonPosX && this.mousePosX < this.playButtonPosX + this.playButtonWidth){
-            if(this.mousePosY > this.playButtonPosY && this.mousePosY < this.playButtonPosY + this.playButtonHeight){
-                this.isHoveringPlay = true;
-            }
-        }
+    playButtonClick() {
+        this.click.play();
+        NewGame();
+        game.scene.stop("MainMenu");
+    }
 
-        if(this.mousePosX > this.questionButtonPosX && this.mousePosX < this.questionButtonPosX + this.questionButtonWidth){
-            if(this.mousePosY > this.questionButtonPosY && this.mousePosY < this.questionButtonPosY + this.questionButtonHeight){
-                this.isHoveringQuestion = true;
-            }
-        }
+    questionButtonClick() {
+        this.click.play();
+        game.scene.start("HowToPlay");
+        game.scene.stop("MainMenu");
+    }
 
-        //apply texture to the play button
-        if(this.isHoveringPlay && this.playButton.texture.key != 'playButtonHover'){
-            this.playButton.setTexture('playButtonHover');
-        }
-        else if(!this.isHoveringPlay && this.playButton.texture.key != 'playButton') {
-            this.playButton.setTexture('playButton');
-        }
+    audioButtonClick() {
+        this.click.play();
 
-        if(this.isHoveringQuestion && this.questionButton.texture.key != 'questionButtonHover'){
-            this.questionButton.setTexture('questionButtonHover');
-        }
-        else if(!this.isHoveringQuestion && this.questionButton.texture.key != 'questionButton') {
-            this.questionButton.setTexture('questionButton');
+        if (audio) {
+            audio = false;
+            playMusic = true;
+            this.music.stop();
+            this.audioButton.setTexture('audioInactiveHover');
+        } else {
+            audio = true;
+            this.music.play();
+            this.audioButton.setTexture('audioActiveHover');
         }
     }
 }
